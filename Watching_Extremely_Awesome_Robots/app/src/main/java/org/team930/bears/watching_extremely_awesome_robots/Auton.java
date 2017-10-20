80905429c042cbee32fc8ee6381f2540bba602d8
theultracorgi <math4ken@gmail.com>
2017-10-19 19:21:05 -0500

    changed parent program

---
+++ Watching_Extremely_Awesome_Robots/app/src/main/java/org/team930/bears/watching_extremely_awesome_robots/Auton.java
@@ -0,0 +1,13 @@
+package org.team930.bears.watching_extremely_awesome_robots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Auton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auton);
+    }
+}
