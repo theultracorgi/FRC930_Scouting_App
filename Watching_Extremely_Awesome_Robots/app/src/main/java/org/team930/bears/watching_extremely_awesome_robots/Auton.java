33e20c30fbc0636bbfe4ee99af6ba2ae9340a6da
theultracorgi <math4ken@gmail.com>
2017-10-30 19:00:02 -0500

    created all widget types

--- Watching_Extremely_Awesome_Robots/app/src/main/java/org/team930/bears/watching_extremely_awesome_robots/Auton.java
+++ Watching_Extremely_Awesome_Robots/app/src/main/java/org/team930/bears/watching_extremely_awesome_robots/Auton.java
@@ -2,12 +2,43 @@ package org.team930.bears.watching_extremely_awesome_robots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Auton extends AppCompatActivity {
-
+    SeekBar sbar;
+    TextView tview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auton);
+        main();
+    }
+
+    public void main(){
+
+        sbar = (SeekBar)findViewById(R.id.numItems);
+        tview = (TextView)findViewById(R.id.displaySeek);
+
+        sbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                tview.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+    public void
+
     }
 }
