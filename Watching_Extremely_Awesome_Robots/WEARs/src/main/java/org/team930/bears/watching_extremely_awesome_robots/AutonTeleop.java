af597c4a15c43a55fec5aebd64e594a4f02d04a7
theultracorgi <math4ken@gmail.com>
2018-01-13 09:42:53 -0600

    Customized for 2018 POWER UPPPPPP

--- Watching_Extremely_Awesome_Robots/WEARs/src/main/java/org/team930/bears/watching_extremely_awesome_robots/AutonTeleop.java
+++ Watching_Extremely_Awesome_Robots/WEARs/src/main/java/org/team930/bears/watching_extremely_awesome_robots/AutonTeleop.java
@@ -8,59 +8,17 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class AutonTeleop extends AppCompatActivity {
-    SeekBar sBar;
-    TextView sBarView, uCounterView;
-    Integer numUnItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autonteleop);
-        numUnItems = 0;
-        main();
-
-    }
-
-    public void main(){
-        sBar = (SeekBar)findViewById(R.id.numItems);
-        sBarView = (TextView)findViewById(R.id.displaySeek);
-        uCounterView = (TextView) findViewById(R.id.displayUnNum);
-        sBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                sBarView.setText(Integer.toString(progress));
-            }
-
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-    }
-
-    public void unlimitedCounterAdd(View v){
-
-        numUnItems = numUnItems + 1;
-
-        uCounterView.setText(Integer.toString(numUnItems));
-    }
-
-    public void unlimitedCounterSubtract(View v){
-
-        numUnItems = numUnItems - 1;
-
-        uCounterView.setText(Integer.toString(numUnItems));
-    }
-
-    public void goToTeleop(View v){
-        Intent nextScreen = new Intent(AutonTeleop.this,PostMatch.class);
-        startActivity(nextScreen);
 
 
     }
 
-}
+
+    }
+
+
 
