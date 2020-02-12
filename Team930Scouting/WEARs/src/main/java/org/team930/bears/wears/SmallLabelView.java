3c1fdbf294db4bf102b9d8ce31f90527e7fc3c72
kennethmathew <math4ken@gmail.com>
2020-02-11 23:08:46 -0600

    continued to add match data metrics

---
+++ Team930Scouting/WEARs/src/main/java/org/team930/bears/wears/SmallLabelView.java
@@ -0,0 +1,63 @@
+package org.team930.bears.wears;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+public class SmallLabelView extends LinearLayout {
+
+    String label;
+    TextView labelView;
+    boolean center;
+
+    public SmallLabelView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeViews(context, attrs);
+
+
+        labelView = findViewById(R.id.labelLabelView);
+
+        if(center) {
+            labelView.setGravity(Gravity.CENTER);
+        }
+
+        labelView.setText(label);
+
+
+    }
+
+
+
+
+
+    private void initializeViews(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.label,
+                0, 0);
+        TypedArray b = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.global,
+                0, 0);
+        this.label = b.getString(R.styleable.global_label);
+        this.center = a.getBoolean(R.styleable.label_center, false);
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.small_label_view, this);
+
+    }
+
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+
+}
