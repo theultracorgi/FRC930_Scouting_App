0f3815d7db51f54796d0ddada7615657015f718c
kennethmathew <math4ken@gmail.com>
2020-02-11 22:21:34 -0600

    did some color stuffs with labels and started making auto/teleop

--- Team930Scouting/WEARs/src/main/java/org/team930/bears/wears/LabelView.java
+++ Team930Scouting/WEARs/src/main/java/org/team930/bears/wears/LabelView.java
@@ -5,26 +5,62 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 public class LabelView extends LinearLayout {
 
     String label;
     TextView labelView;
     boolean center;
+    int stripeColor;
+    ImageView stripe;
 
     public LabelView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initializeViews(context, attrs);
+
 
         labelView = findViewById(R.id.labelLabelView);
 
         if(center) {
             labelView.setGravity(Gravity.CENTER);
         }
+        if(stripeColor !=0) {
+            labelView = findViewById(R.id.labelStripeLabelView);
+        }
+
         labelView.setText(label);
 
+        switch (stripeColor){
+
+            case 1:
+                stripe = findViewById(R.id.labelStripe);
+                stripe.setColorFilter(ContextCompat.getColor(getContext(),R.color.textBlue));
+                break;
+            case 2:
+                stripe = findViewById(R.id.labelStripe);
+                stripe.setColorFilter(ContextCompat.getColor(getContext(),R.color.textRed));
+                break;
+            case 3:
+                stripe = findViewById(R.id.labelStripe);
+                stripe.setColorFilter(ContextCompat.getColor(getContext(),R.color.textPurple));
+                break;
+            case 4:
+                stripe = findViewById(R.id.labelStripe);
+                stripe.setColorFilter(ContextCompat.getColor(getContext(),R.color.textYellow));
+                break;
+            case 5:
+                stripe = findViewById(R.id.labelStripe);
+                stripe.setColorFilter(ContextCompat.getColor(getContext(),R.color.textGreen));
+                break;
+
+            default:
+                break;
+        }
 
 
 
@@ -45,9 +81,16 @@ public class LabelView extends LinearLayout {
                 0, 0);
         this.label = b.getString(R.styleable.global_label);
         this.center = a.getBoolean(R.styleable.label_center, false);
+        this.stripeColor = a.getInt(R.styleable.label_color_text,0);
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.label_view, this);
+
+        if(stripeColor ==0) {
+            inflater.inflate(R.layout.label_view, this);
+        } else {
+            inflater.inflate(R.layout.stripe_label_view, this);
+        }
+
     }
 
     protected void onFinishInflate() {
